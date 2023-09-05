751ba48b552aa1b5fc55f1b7d2f84acfd0ff7666
MichaelRozo <mrozomur@unisangil.edu.co>
2023-09-05 17:45:42 -0500

    Cambio

--- src/main/java/com/mycompany/laboratorio1/Laboratorio1.java
+++ src/main/java/com/mycompany/laboratorio1/Laboratorio1.java
@@ -11,6 +11,6 @@ package com.mycompany.laboratorio1;
 public class Laboratorio1 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello World 2!");
     }
 }
