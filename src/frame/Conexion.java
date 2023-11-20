bf50accb5db90778dece704f5053805b300cb8fa
MichaelRozo <mrozomur@unisangil.edu.co>
2023-11-20 11:24:43 -0500

    Se reliza conexión a BD y se realizan consultas sql

---
+++ src/frame/Conexion.java
@@ -0,0 +1,77 @@
+
+package frame;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+/*import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import javax.swing.DefaultComboBoxModel;
+import javax.swing.JOptionPane;*/
+
+public class Conexion {
+
+    public Connection conectar;
+    public String driver;
+    public String bd;
+    public String usuario;
+    public String password;
+   // Connection con = null;
+    
+    public Conexion(){
+        this.conectar=null;
+        this.driver="com.mysql.jdbc.Driver";
+        this.bd="jdbc:mysql://localhost:3306/rutas_git";
+        this.usuario="root";
+        this.password="";
+    }
+    
+    public Connection procesoConexion(){
+    
+        try{
+            this.conectar = DriverManager.getConnection(this.bd, this.usuario, this.password);
+        
+        }catch(Exception e){
+            System.out.println("Error-> "+e.getMessage());
+        }
+        
+        return this.conectar;
+    }
+    
+    /*public ResultSet consulta(String sql){
+        
+        ResultSet res = null;
+        try {
+            PreparedStatement pstm = con.prepareStatement(sql);
+            res = pstm.executeQuery();
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "Error\n"+e.getMessage(), "¡Error!", JOptionPane.ERROR_MESSAGE);
+        }
+        return res;
+        }
+    
+    public DefaultComboBoxModel mostrarAlumno(){
+        //String grado = lblgrado.getText();
+        DefaultComboBoxModel modelo = new DefaultComboBoxModel();
+        modelo.addElement("Seleccione Alumno");
+        ResultSet res = this.consulta("select * from estudiantes where idGrados=5");
+        
+        try {
+            while(res.next()){
+                modelo.addElement(res.getString("nombreEstud")); 
+            }
+            res.close();
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "Error\n"+e.getMessage(), "¡Error!", JOptionPane.ERROR_MESSAGE);
+        }
+        
+        return modelo;
+    }
+    */
+    
+    
+    
+    
+    
+
+    
+}
---
+++ src/frame/Principal.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package frame;
+
+/**
+ *
+ * @author MICHAEL ROZO
+ */
+public class Principal {
+         public static void main(String[] args) {
+            // Código para iniciar la aplicación
+            login ventana = new login();
+            ventana.setVisible(true);
+}
+
+}
